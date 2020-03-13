fda4129c2df69ece9d1b960dbd6e51abd0b3ae79
Ricardo Celso Micheletto Junior <ricardo.michelettojr@bandtec.com.br>
2020-03-13 10:16:27 -0300

    Criação do formulário de login e conexão com a api

--- webapplication/src/app/login/login.component.ts
+++ webapplication/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm;
 
-  constructor() { }
+  onSubmit(loginData)
+  {
+    this.http.post('http://10.11.112.10:8080/api/auth',loginData).subscribe();
+  }
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.loginForm = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+   }
 
   ngOnInit(): void {
   }
