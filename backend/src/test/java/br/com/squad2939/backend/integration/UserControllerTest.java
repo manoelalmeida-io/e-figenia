e5c3758fb171416fd239b0bbb8f8e41f565c7e70
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-04-07 15:11:30 -0300

    Criando testes de integração para o UserController

--- backend/src/test/java/br/com/squad2939/backend/integration/UserControllerTest.java
+++ backend/src/test/java/br/com/squad2939/backend/integration/UserControllerTest.java
@@ -1,17 +1,74 @@
 package br.com.squad2939.backend.integration;
 
-import br.com.squad2939.backend.controller.UserController;
+import br.com.squad2939.backend.model.User;
+import br.com.squad2939.backend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = UserController.class)
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
-    @Test
-    void listAllUsers() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserService service;
+
+    @Test
+    void listAllUsers() throws Exception {
+        mockMvc.perform(get("/api/users")
+            .contentType("application/json"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void getOneUser() throws Exception {
+        User user = new User();
+        user.setName("John Doeee");
+        user.setEmail("john10@mailserver.com");
+        user.setPassword("password1234");
+        user.setCpf("83728492509");
+        user.setPostalCode("02384150");
+
+        service.create(user);
+        assertTrue(true);
+    }
+
+    @Test
+    void createUser() throws Exception {
+        User user = new User();
+        user.setName("John Doe");
+        user.setEmail("john10@mailserver.com");
+        user.setPassword("password1234");
+        user.setCpf("83728492509");
+        user.setPostalCode("02384150");
+
+        MvcResult result = mockMvc.perform(post("/api/users")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        User created = objectMapper.readValue(response, User.class);
+        assertEquals("John Doe", created.getName());
     }
 }
